e5b0094a283eeb46951519ff81f5832b5683429c
Do Ho <hsnd_1739@q.com>
2022-04-08 21:26:26 -0600

    commit new files

---
+++ openClassBooleanOps.js
@@ -0,0 +1,71 @@
+// Using boolean operators, print true or false if age is greater than or equal to 16. Greater than or equal to operator
+// is >=
+age = 17;
+
+if (age>=16) {
+    console.log('true');
+
+} else {
+    console.log('false');
+  }
+// can also just have one line: console.log(age >= 16);
+
+let x = 45;
+let y = 65;
+let z = 75;
+console.log(x + y > z);
+  // logs out to true
+
+ // Using console.log() and boolean operators, determine if money is greater than or equal to
+ //  the cost of the plane ticket and if vacationTime is true.
+  
+  var planeTicket = 100
+  var money = 105
+  var vacationTime = true;
+  console.log(money >= planeTicket && vacationTime);
+
+  console.log(true && (false || true));
+   // this logs out a true
+
+ // Determine if var1 is not equal to var2. Print the result to the console using console.log().  
+// look at comments below this code too
+ var var1 = 5;
+ var var2 = 6;
+ var isVar1NotEqualVar2 = var1 != var2;
+
+ if (var1 != var2) {
+    console.log(isVar1NotEqualVar2);
+ } else {
+     console.log(isVar1NotEqualVar2);
+ }
+ 
+ // var ageRequiredToDrive = 16;
+ // var currentAge = 17;
+ 
+ // var canPersonDrive = currentAge >= ageRequiredToDrive;
+   // if (currentAge >= ageRequiredToDrive) {
+    // console.log(canPersonDrive);
+ //}
+ 
+ // you also could just have one line of code for above problem: console.log(var1 != var2);
+ var var1 = 5;
+ var var2 = 5;
+  console.log(var1 != var2);
+
+ console.log(45 === "45"); 
+
+ // Determine if myGrade is passing. Using console.log(), print true if it is and false if not.
+// Then, determine if myGrade is an A. Using console.log(), print true or false to the console. (A will be 90 or above).
+
+var myGrade = 56;
+var passingGrade = 70;
+console.log(myGrade >= passingGrade);
+console.log(myGrade >= 90);
+
+var num1 = 16;
+var num2 = 52;
+var num3 = 45;
+console.log(num1 >= num2 && num3 > num2);
+ // this logs out to false
+
+ 
